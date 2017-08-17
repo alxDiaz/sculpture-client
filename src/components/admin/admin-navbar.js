441fe548ef8232f90a1e3df49d9cbae9d689572e
Eduard Trias <triaseduard@gmail.com>
2017-08-17 17:15:22 +0200

    add admin and menu components

---
+++ src/components/admin/admin-navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AdminNavbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>Families</li>
+        <li>Media</li>
+        <li>Expositions</li>
+        <li>Users</li>
+      </ul>
+    </div>
+  )
+}
+
+export default AdminNavbar;
---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Admin from './admin/admin';
+
+const App = () => {
+  return (
+    <div>
+      <Admin />
+    </div>
+  )
+}
+
+export default App;
