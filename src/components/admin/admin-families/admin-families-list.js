8c94aa49d1e4722fec8aee349036760251bb0f1f
Eduard Trias <triaseduard@gmail.com>
2017-08-18 22:53:35 +0200

    add admin-create-families and admin-families-list components with its POST and GET requests

--- src/components/admin/admin-families/admin-families-list.js
+++ src/components/admin/admin-families/admin-families-list.js
@@ -1,23 +1,32 @@
 import React from 'react';
+import axios from 'axios';
 
 class AdminFamiliesList extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      term: []
+      families: []
     }
+    this.api = 'http://localhost:27017/api/families';
+  }
 
+  componentWillMount() {
+    axios.get(this.api)
+      // .then(res => console.log(res.data))
+      .then(res => this.setState({families: res.data}))
+      .catch(error => console.log(error))
   }
 
   render() {
     return (
-      <div>
-        This is families list
+      <div className="admin-families-list">
+        <ul>
+          {this.state.families.map(item => <li key={item._id}>{item.title}</li>)}
+        </ul>
       </div>
     )
   }
 }
-
 
 export default AdminFamiliesList;
