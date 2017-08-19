a7d471403112505f47d572c19b259a34edde830a
Eduard Trias <triaseduard@gmail.com>
2017-08-19 17:46:04 +0200

    add send api props to child component admin-families-list-item

--- src/components/admin/admin-families/admin-families-list.js
+++ src/components/admin/admin-families/admin-families-list.js
@@ -1,34 +1,23 @@
 import React from 'react';
-import axios from 'axios';
 
 import AdminFamiliesListItem from './admin-families-list-item';
 
-class AdminFamiliesList extends React.Component {
-  constructor(props) {
-    super(props);
+const AdminFamiliesList = (props) => {
 
-    this.state = {
-      families: []
-    }
-    this.api = 'http://localhost:27017/api/families';
-  }
+  const familyItems = props.families.map((item) => {
+    console.log(item)
+    return <AdminFamiliesListItem key={item._id} family={item} />
+  })
 
-  componentWillMount() {
-    axios.get(this.api)
-      // .then(res => console.log(res.data))
-      .then(res => this.setState({families: res.data}))
-      .catch(error => console.log(error))
-  }
-
-  render() {
-    return (
-      <div className="admin-families-list">
-        <ul>
-          {this.state.families.map(item => <li key={item._id}>{item.title}</li>)}
-        </ul>
-      </div>
-    )
-  }
+  return (
+    <div className="admin-families-list">
+      <ul>
+        {familyItems}
+      </ul>
+    </div>
+  )
 }
+
+
 
 export default AdminFamiliesList;
