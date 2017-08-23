22555c576b684435301587781aaf602594647b0f
Eduard Trias <triaseduard@gmail.com>
2017-08-23 10:55:59 +0200

    void commit

--- src/components/admin/admin-families/admin-artworks/admin-create-artworks.js
+++ src/components/admin/admin-families/admin-artworks/admin-create-artworks.js
@@ -10,6 +10,7 @@ class AdminCreateArtworks extends React.Component {
             family: this.props.family._id,
             material: '',
             specs: '',
+            image: '',
             // image: '',
             // fieldname: '',
             // originalname: '',
@@ -24,23 +25,26 @@ class AdminCreateArtworks extends React.Component {
         this.api = 'http://localhost:27017/api/artworks';
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChangeFile = this.handleChangeFile.bind(this);
     }
 
     handleSubmit(event) {
-
+      event.preventDefault();
+      console.log(this.state.image)
       axios.post(this.api, {
         name: this.state.name,
+        family: this.state.family,
         material: this.state.material,
         specs: this.state.specs,
-        // image: event.target.file,
-        // fieldname: event.target.file.fieldname,
-        // originalname: event.target.file.originalname,
-        // encoding: event.target.file.encoding,
-        // mimetype: event.target.file.mimetype,
-        // destination: event.target.file.destination,
-        // filename: event.target.file.filename,
-        // path: event.target.file.path,
-        // size: event.target.file.size
+        image: this.state.image,
+        fieldname: event.target.file.fieldname,
+        originalname: event.target.file.originalname,
+        encoding: event.target.file.encoding,
+        mimetype: event.target.file.mimetype,
+        destination: event.target.file.destination,
+        filename: event.target.file.filename,
+        path: event.target.file.path,
+        size: event.target.file.size
       })
       .then(res => console.log(res))
       .catch(error => console.log(error))
@@ -50,10 +54,27 @@ class AdminCreateArtworks extends React.Component {
         return this.setState({[event.target.name]: event.target.value});
     }
 
-    handleUploadFile(event) {
-      const data = new FormData();
-      return this.setState({image: data.append('image', event.target.files[0])})
+    handleChangeFile(event) {
+      this.setState({image: event.target.files[0]})
     }
+
+  //PUPURRI FINISHES
+  //---------------------------------------------------------
+    // handleChangeFile(event) {
+    //
+    //   event.preventDefault();
+    //
+    // let reader = new FileReader();
+    // let file = event.target.files[0];
+    //
+    // reader.onloadend = () => {
+    //   this.setState({
+    //     image: file,
+    //   });
+    // }
+    //
+    // reader.readAsDataURL(file)
+    // }
 
     render(){
         return (
@@ -62,11 +83,9 @@ class AdminCreateArtworks extends React.Component {
                   <input type="text" placeholder="Name" name="name" value={this.state.name} onChange={this.handleChange} />
                   <input type="text" placeholder="Material" name="material" value={this.state.material} onChange={this.handleChange} />
                   <input type="text" placeholder="Specs" name="specs" value={this.state.specs} onChange={this.handleChange} />
-                  <input type="file" ref="file" name="image" value={this.state.image} onChange={this.handleChange}/>
+                  <input type="file" ref="file" name="image" onChange={this.handleChangeFile}/>
                   <input type="submit" value="Save"/>
                  </form>
-                <p>{this.state.name}</p>
-                {console.log("hola")}
             </div>
         )
     };
