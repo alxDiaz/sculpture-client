4fb7b3db72786d6d231b4ee7392ddbc41bffa06b
Alex Diaz <diaz.infante@gmail.com>
2017-08-23 10:15:08 +0200

    Send files from Client to Server

--- src/components/admin/admin-contact/admin-contact.js
+++ src/components/admin/admin-contact/admin-contact.js
@@ -41,6 +41,8 @@ class AdminContact extends React.Component {
   }
 
   handleSubmit(event) {
+    event.preventDefault();
+    
     axios.put(`${this.api}/${this.state._id}`, {
     name: this.state.name,
     email: this.state.email,
